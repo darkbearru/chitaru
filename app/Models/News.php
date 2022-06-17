ff8ce0873a05edc9f02c0afe3eddf5541ad6d12e
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-17 20:21:16 +0900

    Add. Добавление в модель News получения списка последних данных, а также форматирование времени

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,17 +14,34 @@ class News extends Model
 
     protected $guarded = [];
 
-    public function type (): BelongsTo
+    public static function Latest(): Collection
+    {
+        return self::DateFormat(self::with(['type'])->get());
+//        return self::with(['type', 'region', 'category'])->paginate(10);
+    }
+
+    protected static function DateFormat(Collection $list): Collection
+    {
+        foreach ($list as $item) {
+            $item->published_at =
+                Carbon::createFromFormat('Y-m-d H:i:s', $item->published_at)
+                    ->locale('ru')
+                    ->isoFormat('Do MMMM, HH:mm');
+        }
+        return $list;
+    }
+
+    public function type(): BelongsTo
     {
         return $this->belongsTo(NewsType::class);
     }
 
-    public function region () : BelongsTo
+    public function region(): BelongsTo
     {
         return $this->belongsTo(Region::class);
     }
 
-    public function category () : BelongsTo
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
