db571a926610ac911b095fe273f998803239d126
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-24 11:04:49 +0900

    Update. Удаление наполнения новостей т.к. они импортируются

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use App\Models\News;
 use App\Models\NewsType;
 use App\Models\Region;
 use Illuminate\Database\Seeder;
@@ -22,6 +21,6 @@ class DatabaseSeeder extends Seeder
         NewsType::factory(5)->create();
         Region::factory(40)->create();
         Category::factory(30)->create();
-        News::factory(15)->create();
+        //News::factory(15)->create();
     }
 }
